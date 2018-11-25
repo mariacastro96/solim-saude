8a3f2cacb770af79c7efc2944f2336aa99ed562c
Maria Castro <mariacastro@aquaporservicos.pt>
2018-11-25 23:02:29 +0000

    #13 footer done + languages are now changeable in footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
+  before_action :set_current_language
 
   private
+
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def set_current_language
+    Current.language = Language.find_by(language_id: I18n.locale)
   end
 
 end
